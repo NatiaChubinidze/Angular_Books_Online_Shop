0b6799285c9df996b83395c7e7cd0d6aa67e461c
NatiaChubinidze <chubinidze.natia97@gmail.com>
2021-05-01 17:45:39 +0400

    update:cards hover. feat: wishlist-readBooks filter functionality

--- Books-Online-Shop/src/app/wishlist/wishlist.component.ts
+++ Books-Online-Shop/src/app/wishlist/wishlist.component.ts
@@ -1,54 +1,98 @@
 import { Component, OnInit } from '@angular/core';
 
-import { IBooks } from '../books-search/shared/interfaces/books-response.interface';
 import { IFirebaseBook } from '../shared/interfaces/firebase-book.interface';
 import { FireBaseCrudService } from '../shared/services/firebase-crud/firebase-crud.service';
 
 @Component({
   selector: 'app-wishlist',
   templateUrl: './wishlist.component.html',
-  styleUrls: ['./wishlist.component.scss']
+  styleUrls: ['./wishlist.component.scss'],
 })
 export class WishlistComponent implements OnInit {
-  
-  books:IFirebaseBook[];
+  private _searchTitle: string;
+  private _searchAuthor: string;
+  books: IFirebaseBook[]=[];
+  filteredBooks: IFirebaseBook[]=[];
 
-  constructor(private _firebaseCrudService:FireBaseCrudService) { }
+  constructor(private _firebaseCrudService: FireBaseCrudService) {}
+
+
+  get searchTitle(): string {
+    return this._searchTitle;
+
+  }
+  get searchAuthor(): string {
+    return this._searchAuthor;
+  }
+  set searchTitle(value: string) {
+    this._searchTitle = value;
+    if (this._searchTitle) {
+      this.filteredBooks = this.books.filter((book) =>{
+        return (book.title.toLowerCase().includes(this._searchTitle.toLowerCase()))
+      }
+      );
+      console.log(this.filteredBooks);
+    } else {
+      this.filteredBooks = this.books.slice();
+    }
+  }
+  set searchAuthor(value: string) {
+    this._searchAuthor = value;
+    if (this._searchAuthor) {
+      this.filteredBooks = this.books.filter((book) =>
+        book.author.toLowerCase().includes(this._searchAuthor.toLowerCase())
+      );
+      console.log(this.filteredBooks);
+    } else {
+      this.filteredBooks = this.books.slice();
+    }
+  }
+
+  addToRead(book: IFirebaseBook) {
+    const bookToAdd: IFirebaseBook = {
+      title: book.title,
+      author: book.author,
+      price: book.price,
+      thumbnail: book.thumbnail,
+      subject: book.subject,
+      quantity: book.quantity,
+    };
+    this._firebaseCrudService.saveItem('books-read', bookToAdd);
+    this._firebaseCrudService.deleteItem('wishlist', book.id);
+    this.filteredBooks=this.filteredBooks.filter(item=>item.id!==book.id);
+  }
+  addToCart(book: IFirebaseBook) {
+    const bookToAdd: IFirebaseBook = {
+      title: book.title,
+      author: book.author,
+      price: book.price,
+      thumbnail: book.thumbnail,
+      subject: book.subject,
+      quantity: book.quantity,
+    };
+    this._firebaseCrudService.saveItem('shopping-cart', bookToAdd);
+  }
+  
+  deleteFromWishlist(book: IFirebaseBook) {
+    this._firebaseCrudService.deleteItem('wishlist', book.id);
+    this.filteredBooks=this.filteredBooks.filter(item=>item.id!==book.id);
+
+  }
 
   ngOnInit(): void {
-    this._firebaseCrudService.getCollection("wishlist").subscribe((books:any)=>{
-      if(books){
-        this.books=this._firebaseCrudService.wishlist=books;
-        console.log(this.books);
-      }
-    })
+    this._firebaseCrudService
+      .getCollection('wishlist')
+      .subscribe((books: any) => {
+        if (books) {
+          this.books = this._firebaseCrudService.wishlist= books;
+          console.log(this.books);
+        }
+      });
+      setTimeout(()=>{
+        if(this.books){
+          this.searchTitle = '';
+          this.searchAuthor = '';
+        }
+      },1000);
   }
-
-  addToRead(book:IFirebaseBook){
-    const bookToAdd:IFirebaseBook={
-      title:book.title,
-      author:book.author,
-      price:book.price,
-      thumbnail:book.thumbnail,
-      subject:book.subject,
-      quantity:book.quantity,
-    }
-    this._firebaseCrudService.saveItem("books-read",bookToAdd);
-    this._firebaseCrudService.deleteItem("wishlist",book.id);
-  }
-  addToCart(book:IFirebaseBook){
-    const bookToAdd:IFirebaseBook={
-      title:book.title,
-      author:book.author,
-      price:book.price,
-      thumbnail:book.thumbnail,
-      subject:book.subject,
-      quantity:book.quantity,
-    }
-    this._firebaseCrudService.saveItem("shopping-cart",bookToAdd);
-  }
-  deleteFromWishlist(book:IFirebaseBook){
-    this._firebaseCrudService.deleteItem("wishlist",book.id);
-  }
-
 }
--- Books-Online-Shop/src/app/wishlist/wishlist.module.ts
+++ Books-Online-Shop/src/app/wishlist/wishlist.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { WishlistComponent } from './wishlist.component';
 import { BookCardsModule } from '../shared/components/book-cards.module';
+
 
 
 
 @NgModule({
   declarations: [WishlistComponent],
   imports: [
-    CommonModule,BookCardsModule
+    CommonModule,BookCardsModule,FormsModule
   ]
 })
 export class WishlistModule { }
