c6a9c46693fb7dc3cfb0f8dfdd200c54b92cf1d2
NatiaChubinidze <chubinidze.natia97@gmail.com>
2021-05-04 18:28:28 +0400

    admin functionality

--- Books-Online-Shop/src/app/app-routing.module.ts
+++ Books-Online-Shop/src/app/app-routing.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-{path:'',
-redirectTo:'sign-in',
-pathMatch:'full'
-}
+  { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
