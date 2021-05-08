ba0bcd7e87a86a80b1a32f3761328f045f697e3b
NatiaChubinidze <chubinidze.natia97@gmail.com>
2021-05-08 15:56:27 +0400

    update: refactor dashboards

---
+++ Books-Online-Shop/src/app/admin-panel/shared/components/pie-chart/pie-chart.component.ts
@@ -0,0 +1,57 @@
+import { Component, Input, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import { ITopBooks } from '../../interfaces/top-books.interface';
+
+@Component({
+  selector: 'app-pie-chart',
+  templateUrl: './pie-chart.component.html',
+  styleUrls: ['./pie-chart.component.scss']
+})
+export class PieChartComponent implements OnInit {
+  @Input() topBooks: ITopBooks[] = [];
+  @Input() chartText:string='';
+  chartOptions = {};
+
+  Highcharts = Highcharts;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.chartOptions = {
+      chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie',
+      },
+      title: {
+        text: this.chartText,
+      },
+      tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>',
+      },
+      accessibility: {
+        point: {
+          valueSuffix: '%',
+        },
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: true,
+            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+          },
+        },
+      },
+      series: [
+        {
+          name: 'Brands',
+          colorByPoint: true,
+          data: [...this.topBooks],
+        },
+      ],
+    };
+  }
+
+}
