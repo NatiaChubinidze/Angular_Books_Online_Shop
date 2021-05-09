2ad3a2b1ae33872a96f9c390df7b7ab54c896658
NatiaChubinidze <chubinidze.natia97@gmail.com>
2021-05-09 11:08:48 +0400

    feat: page not found

--- Books-Online-Shop/src/app/admin-panel/outline/admin-panel.component.ts
+++ Books-Online-Shop/src/app/admin-panel/outline/admin-panel.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import * as Highcharts from 'highcharts';
 import { TOKEN_EXP_KEY, TOKEN_KEY } from '../../shared/constants/constants';
 import { FirebaseAuthService } from '../../shared/services/firebase-auth/firebase-auth.service';
 
